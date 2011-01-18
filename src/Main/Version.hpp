5261f371daccc0c34e0a8a5a8b34070ba4242214
David C. Bishop <david@davidbishop.org>
2011-01-18 15:15:42 +1100

    Added extra check for missing version information to sample hpp header file.

--- src/Main/Version.hpp
+++ src/Main/Version.hpp
@@ -1,6 +1,16 @@
 #ifndef VERSION_HPP_
 #define VERSION_HPP_
 
+#ifndef _BUILD_DATE
+#define _BUILD_DATE "UNKNOWN"
+#endif /* _BUILD_DATE */
+#ifndef _GIT_VERSION
+#define _GIT_VERSION "UNKNOWN"
+#endif /* _GIT_VERSION */
+#ifndef _GIT_SHA1
+#define _GIT_SHA1 "UNKNOWN"
+#endif /* _GIT_SHA1 */
+
 #include <string>
 using namespace std;
 
