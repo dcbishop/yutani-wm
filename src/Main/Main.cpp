ad0faa6f57fd44067a6b44e78d7542c8076faa48
David C. Bishop <david@davidbishop.org>
2012-08-14 22:12:51 +1000

    Now uses hpp.in file for build configures. CMake improvements.

--- src/Main/Main.cpp
+++ src/Main/Main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 #include "../Debug/console.h"
 
 int main(int argc, char* argv[]) {
-   LOG("Starting %s, version %s %s, built: %s...", 
-   	argv[0],
-   	globals.getGitVersion().c_str(),
-   	globals.getBuildType().c_str(),
-   	globals.getBuildDate().c_str()
+   LOG("%s: %s, version %s %s, built: %s...", 
+      argv[0],
+      Globals::getProjectName().c_str(),
+      Globals::getGitVersion().c_str(),
+      Globals::getBuildType().c_str(),
+      Globals::getBuildDate().c_str()
    );
    DEBUG_A("Debug: ON");
 
